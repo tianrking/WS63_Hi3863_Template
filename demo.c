e75dc212a9676e26f8bd623d5e85b2505a8d25a2
tianrking <tian.r.king@gmail.com>
2024-09-10 15:08:25 +0800

    modified:   demo.c

--- demo.c
+++ demo.c
@@ -1,140 +1,134 @@
+
+#define CONFIG_UART_BUS_ID 1
+#define CONFIG_UART_RXD_PIN 16
+#define CONFIG_UART_TXD_PIN 15
+
 #include "pinctrl.h"
 #include "soc_osal.h"
-#include "pwm.h"
-#include "gpio.h"
+#include "uart.h"
+#include "watchdog.h"
 #include "osal_debug.h"
 #include "cmsis_os2.h"
 #include "app_init.h"
 
-#define PWM_GPIO                   2
-#define PWM_PIN_MODE               1
-#define PWM_CHANNEL                2
-#define PWM_GROUP_ID               0
-#define PWM_FREQUENCY              1000  // 1kHz
-#define PWM_TASK_STACK_SIZE        0x1000
-#define PWM_TASK_DURATION_MS       1000
-#define PWM_TASK_PRIO              (osPriority_t)(17)
+#define UART_BAUDRATE 115200
+#define UART_DATA_BITS 3
+#define UART_STOP_BITS 1
+#define UART_PARITY_BIT 0
+#define UART_TRANSFER_SIZE 512
+#define CONFIG_UART_INT_WAIT_MS 5
 
-static volatile uint32_t g_pwm_duty_cycle = 30;  // Initial duty cycle 30%
+#define UART_TASK_STACK_SIZE 0x1000
+#define UART_TASK_DURATION_MS 1000
+#define UART_TASK_PRIO (osPriority_t)(17)
 
-static void app_pwm_init_pin(void)
+static uint8_t g_app_uart_rx_buff[UART_TRANSFER_SIZE] = {0};
+
+static uint8_t g_app_uart_int_rx_flag = 0;
+
+static uart_buffer_config_t g_app_uart_buffer_config = {
+    .rx_buffer = g_app_uart_rx_buff,
+    .rx_buffer_size = UART_TRANSFER_SIZE};
+
+static void app_uart_init_pin(void)
 {
-    uapi_pin_set_mode(PWM_GPIO, PWM_PIN_MODE);
+    uapi_pin_set_mode(CONFIG_UART_TXD_PIN, PIN_MODE_1);
+    uapi_pin_set_mode(CONFIG_UART_RXD_PIN, PIN_MODE_1);
 }
 
-static void app_pwm_init_config(void)
+static void app_uart_init_config(void)
 {
-    pwm_config_t config = {0};
-    uint32_t pwm_clock = uapi_pwm_get_frequency(PWM_CHANNEL);
-    uint32_t pwm_period = pwm_clock / PWM_FREQUENCY;
-    uint32_t pwm_high_time = (pwm_period * g_pwm_duty_cycle) / 100;
-    uint32_t pwm_low_time = pwm_period - pwm_high_time;
+    uart_attr_t attr = {
+        .baud_rate = UART_BAUDRATE,
+        .data_bits = UART_DATA_BITS,
+        .stop_bits = UART_STOP_BITS,
+        .parity = UART_PARITY_BIT};
 
-    config.low_time = pwm_low_time;
-    config.high_time = pwm_high_time;
-    config.offset_time = 0;
-    config.cycles = 0;
-    config.repeat = true;
+    uart_pin_config_t pin_config = {
+        .tx_pin = CONFIG_UART_TXD_PIN,
+        .rx_pin = CONFIG_UART_RXD_PIN,
+        .cts_pin = PIN_NONE,
+        .rts_pin = PIN_NONE};
+    uapi_uart_deinit(CONFIG_UART_BUS_ID);
+    uapi_uart_init(CONFIG_UART_BUS_ID, &pin_config, &attr, NULL, &g_app_uart_buffer_config);
+}
 
-    errcode_t ret = uapi_pwm_init();
-    if (ret != ERRCODE_SUCC) {
-        osal_printk("Failed to initialize PWM, error code: %d\r\n", ret);
+static void app_uart_read_int_handler(const void *buffer, uint16_t length, bool error)
+{
+    unused(error);
+    if (buffer == NULL || length == 0)
+    {
+        osal_printk("uart%d int mode transfer illegal data!\r\n", CONFIG_UART_BUS_ID);
         return;
     }
 
-    ret = uapi_pwm_open(PWM_CHANNEL, &config);
-    if (ret != ERRCODE_SUCC) {
-        osal_printk("Failed to open PWM channel, error code: %d\r\n", ret);
+    uint8_t *buff = (uint8_t *)buffer;
+    if (memcpy_s(g_app_uart_rx_buff, length, buff, length) != EOK)
+    {
+        osal_printk("uart%d int mode data copy fail!\r\n", CONFIG_UART_BUS_ID);
         return;
     }
-
-    uint8_t channel_id = PWM_CHANNEL;
-    ret = uapi_pwm_set_group(PWM_GROUP_ID, &channel_id, 1);
-    if (ret != ERRCODE_SUCC) {
-        osal_printk("Failed to set PWM group, error code: %d\r\n", ret);
-        return;
-    }
+    g_app_uart_int_rx_flag = 1;
 }
 
-static errcode_t pwm_update_duty_cycle(uint32_t duty_cycle)
+static void app_uart_write_int_handler(const void *buffer, uint32_t length, const void *params)
 {
-    uint32_t pwm_clock = uapi_pwm_get_frequency(PWM_CHANNEL);
-    uint32_t pwm_period = pwm_clock / PWM_FREQUENCY;
-    uint32_t pwm_high_time = (pwm_period * duty_cycle) / 100;
-    uint32_t pwm_low_time = pwm_period - pwm_high_time;
-
-    pwm_config_t cfg = {
-        pwm_low_time,
-        pwm_high_time,
-        0,
-        0,
-        true
-    };
-
-    errcode_t ret = uapi_pwm_open(PWM_CHANNEL, &cfg);
-    if (ret != ERRCODE_SUCC) {
-        osal_printk("Failed to reconfigure PWM, error code: %d\r\n", ret);
-        return ret;
+    unused(params);
+    uint8_t *buff = (void *)buffer;
+    for (uint32_t i = 0; i < length; i++)
+    {
+        osal_printk("uart%d write data[%d] = %d\r\n", CONFIG_UART_BUS_ID, i, buff[i]);
     }
-
-    osal_printk("PWM duty cycle updated to %lu%%\r\n", duty_cycle);
-    return ERRCODE_SUCC;
 }
 
-static void *pwm_task(const char *arg)
+static void *uart_task(const char *arg)
 {
     unused(arg);
+    /* UART pinmux. */
+    app_uart_init_pin();
 
-    /* PWM pinmux. */
-    app_pwm_init_pin();
+    /* UART init config. */
+    app_uart_init_config();
 
-    /* PWM init config. */
-    app_pwm_init_config();
-
-    errcode_t ret = uapi_pwm_start_group(PWM_GROUP_ID);
-    if (ret != ERRCODE_SUCC) {
-        osal_printk("Failed to start PWM group, error code: %d\r\n", ret);
-        return NULL;
+    osal_printk("uart%d int mode register receive callback start!\r\n", CONFIG_UART_BUS_ID);
+    if (uapi_uart_register_rx_callback(CONFIG_UART_BUS_ID, UART_RX_CONDITION_FULL_OR_IDLE,
+                                       1, app_uart_read_int_handler) == ERRCODE_SUCC)
+    {
+        osal_printk("uart%d int mode register receive callback succ!\r\n", CONFIG_UART_BUS_ID);
     }
 
-    osal_printk("PWM started with %lu%% duty cycle\r\n", g_pwm_duty_cycle);
+    while (1)
+    {
+        osal_msleep(UART_TASK_DURATION_MS);
 
-    uint32_t last_duty_cycle = g_pwm_duty_cycle;
-
-    while (1) {
-        osal_msleep(PWM_TASK_DURATION_MS);
-        
-        if (g_pwm_duty_cycle != last_duty_cycle) {
-            ret = pwm_update_duty_cycle(g_pwm_duty_cycle);
-            if (ret == ERRCODE_SUCC) {
-                last_duty_cycle = g_pwm_duty_cycle;
-            }
+        while (g_app_uart_int_rx_flag != 1)
+        {
+            osal_msleep(CONFIG_UART_INT_WAIT_MS);
         }
-        
-        osal_printk("PWM running, current duty cycle: %lu%%\r\n", last_duty_cycle);
+        g_app_uart_int_rx_flag = 0;
+        osal_printk("uart%d int mode send back!\r\n", CONFIG_UART_BUS_ID);
+        if (uapi_uart_write_int(CONFIG_UART_BUS_ID, g_app_uart_rx_buff, UART_TRANSFER_SIZE, 0,
+                                app_uart_write_int_handler) == ERRCODE_SUCC)
+        {
+            osal_printk("uart%d int mode send back succ!\r\n", CONFIG_UART_BUS_ID);
+        }
     }
 
     return NULL;
 }
 
-static void pwm_entry(void)
+static void uart_entry(void)
 {
-    osThreadAttr_t attr;
-
-    attr.name = "PWMTask";
-    attr.attr_bits = 0U;
-    attr.cb_mem = NULL;
-    attr.cb_size = 0U;
-    attr.stack_mem = NULL;
-    attr.stack_size = PWM_TASK_STACK_SIZE;
-    attr.priority = PWM_TASK_PRIO;
-
-    if (osThreadNew((osThreadFunc_t)pwm_task, NULL, &attr) == NULL) {
-        osal_printk("Failed to create PWM task\r\n");
-    } else {
-        osal_printk("PWM task created successfully\r\n");
+    osal_task *task_handle = NULL;
+    osal_kthread_lock();
+    task_handle = osal_kthread_create((osal_kthread_handler)uart_task, 0, "UartTask", UART_TASK_STACK_SIZE);
+    if (task_handle != NULL)
+    {
+        osal_kthread_set_priority(task_handle, UART_TASK_PRIO);
+        osal_kfree(task_handle);
     }
+    osal_kthread_unlock();
 }
 
-/* Run the pwm_entry. */
-app_run(pwm_entry);
+/* Run the uart_entry. */
+app_run(uart_entry);
