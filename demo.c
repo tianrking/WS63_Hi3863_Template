0222c6e272238437c097fe1fbb40949a341ffdb1
tianrking <tian.r.king@gmail.com>
2024-09-10 15:36:06 +0800

    modified:   demo.c

--- demo.c
+++ demo.c
@@ -8,105 +8,272 @@
 #include "cmsis_os2.h"
 #include "app_init.h"
 #include "soc_osal.h"
+#include "lwip/ip_addr.h"
 
-#define WIFI_IFNAME_MAX_SIZE            16
-#define WIFI_MAX_KEY_LEN                65
-#define WIFI_MAX_SSID_LEN               33
-#define WIFI_SOFTAP_SAMPLE_LOG          "[WIFI_SOFTAP_SAMPLE]"
+#define WIFI_IFNAME_MAX_SIZE             16
+#define WIFI_MAX_SSID_LEN                33
+#define WIFI_SCAN_AP_LIMIT               64
+#define WIFI_MAC_LEN                     6
+#define WIFI_STA_SAMPLE_LOG              "[WIFI_STA_SAMPLE]"
+#define WIFI_NOT_AVALLIABLE              0
+#define WIFI_AVALIABE                    1
+#define WIFI_GET_IP_MAX_COUNT            300
 
 #define WIFI_TASK_PRIO                  (osPriority_t)(13)
 #define WIFI_TASK_DURATION_MS           2000
 #define WIFI_TASK_STACK_SIZE            0x1000
-/*****************************************************************************
-  SoftAP sample用例
-*****************************************************************************/
-td_s32 example_softap_function(td_void)
+
+static td_void wifi_scan_state_changed(td_s32 state, td_s32 size);
+static td_void wifi_connection_changed(td_s32 state, const wifi_linked_info_stru *info, td_s32 reason_code);
+
+wifi_event_stru wifi_event_cb = {
+    .wifi_event_connection_changed      = wifi_connection_changed,
+    .wifi_event_scan_state_changed      = wifi_scan_state_changed,
+};
+
+enum {
+    WIFI_STA_SAMPLE_INIT = 0,       /* 0:初始态 */
+    WIFI_STA_SAMPLE_SCANING,        /* 1:扫描中 */
+    WIFI_STA_SAMPLE_SCAN_DONE,      /* 2:扫描完成 */
+    WIFI_STA_SAMPLE_FOUND_TARGET,   /* 3:匹配到目标AP */
+    WIFI_STA_SAMPLE_CONNECTING,     /* 4:连接中 */
+    WIFI_STA_SAMPLE_CONNECT_DONE,   /* 5:关联成功 */
+    WIFI_STA_SAMPLE_GET_IP,         /* 6:获取IP */
+} wifi_state_enum;
+
+static td_u8 g_wifi_state = WIFI_STA_SAMPLE_INIT;
+
+static td_void wifi_scan_state_changed(td_s32 state, td_s32 size)
 {
-    /* SoftAp接口的信息 */
-    td_char ssid[WIFI_MAX_SSID_LEN] = "H3863";
-    td_char pre_shared_key[WIFI_MAX_KEY_LEN] = "22222222";
-    softap_config_stru hapd_conf = {0};
-    softap_config_advance_stru config = {0};
-    td_char ifname[WIFI_IFNAME_MAX_SIZE + 1] = "ap0"; /* 创建的SoftAp接口名 */
-    struct netif *netif_p = TD_NULL;
-    ip4_addr_t   st_gw;
-    ip4_addr_t   st_ipaddr;
-    ip4_addr_t   st_netmask;
-    IP4_ADDR(&st_ipaddr, 192, 168, 43, 1); /* IP地址设置：192.168.43.1 */
-    IP4_ADDR(&st_netmask, 255, 255, 255, 0); /* 子网掩码设置：255.255.255.0 */
-    IP4_ADDR(&st_gw, 192, 168, 43, 2); /* 网关地址设置：192.168.43.2 */
+    UNUSED(state);
+    UNUSED(size);
+    PRINT("%s::Scan done!.\r\n", WIFI_STA_SAMPLE_LOG);
+    g_wifi_state = WIFI_STA_SAMPLE_SCAN_DONE;
+    return;
+}
 
-    /* 配置SoftAp基本参数 */
-    (td_void)memcpy_s(hapd_conf.ssid, sizeof(hapd_conf.ssid), ssid, sizeof(ssid));
-    (td_void)memcpy_s(hapd_conf.pre_shared_key, WIFI_MAX_KEY_LEN, pre_shared_key, WIFI_MAX_KEY_LEN);
-    hapd_conf.security_type = 3; /* 3: 加密方式设置为WPA_WPA2_PSK */
-    hapd_conf.channel_num = 13; /* 13：工作信道设置为13信道 */
-    hapd_conf.wifi_psk_type = 0;
+static td_void wifi_connection_changed(td_s32 state, const wifi_linked_info_stru *info, td_s32 reason_code)
+{
+    UNUSED(info);
+    UNUSED(reason_code);
 
-    /* 配置SoftAp网络参数 */
-    config.beacon_interval = 100; /* 100：Beacon周期设置为100ms */
-    config.dtim_period = 2; /* 2：DTIM周期设置为2 */
-    config.gi = 0; /* 0：short GI默认关闭 */
-    config.group_rekey = 86400; /* 86400：组播秘钥更新时间设置为1天 */
-    config.protocol_mode = 4; /* 4：协议类型设置为802.11b + 802.11g + 802.11n + 802.11ax */
-    config.hidden_ssid_flag = 1; /* 1：不隐藏SSID */
-    if (wifi_set_softap_config_advance(&config) != 0) {
+    if (state == WIFI_NOT_AVALLIABLE) {
+        PRINT("%s::Connect fail!. try agin !\r\n", WIFI_STA_SAMPLE_LOG);
+        g_wifi_state = WIFI_STA_SAMPLE_INIT;
+    } else {
+        PRINT("%s::Connect succ!.\r\n", WIFI_STA_SAMPLE_LOG);
+        g_wifi_state = WIFI_STA_SAMPLE_CONNECT_DONE;
+    }
+}
+
+static void print_ip_address(void)
+{
+    struct netif *netif = NULL;
+    ip4_addr_t ip_addr;
+    ip4_addr_t netmask;
+    ip4_addr_t gw;
+
+    netif = netifapi_netif_find("wlan0");
+    if (netif == NULL) {
+        PRINT("Failed to find netif\r\n");
+        return;
+    }
+
+    ip_addr = netif->ip_addr.u_addr.ip4;
+    netmask = netif->netmask.u_addr.ip4;
+    gw = netif->gw.u_addr.ip4;
+
+    PRINT("IP address: %s\r\n", ip4addr_ntoa(&ip_addr));
+    PRINT("Netmask: %s\r\n", ip4addr_ntoa(&netmask));
+    PRINT("Gateway: %s\r\n", ip4addr_ntoa(&gw));
+}
+
+td_s32 example_get_match_network(wifi_sta_config_stru *expected_bss)
+{
+    td_s32  ret;
+    td_u32  num = 64; /* 64:扫描到的Wi-Fi网络数量 */
+    td_char expected_ssid[] = "KUNIU";
+    td_char key[] = "kuniu666"; /* 待连接的网络接入密码 */
+    td_bool find_ap = TD_FALSE;
+    td_u8   bss_index;
+    /* 获取扫描结果 */
+    td_u32 scan_len = sizeof(wifi_scan_info_stru) * WIFI_SCAN_AP_LIMIT;
+    wifi_scan_info_stru *result = osal_kmalloc(scan_len, OSAL_GFP_ATOMIC);
+    if (result == TD_NULL) {
         return -1;
     }
-    /* 启动SoftAp接口 */
-    if (wifi_softap_enable(&hapd_conf) != 0) {
+    memset_s(result, scan_len, 0, scan_len);
+    ret = wifi_sta_get_scan_info(result, &num);
+    if (ret != 0) {
+        osal_kfree(result);
         return -1;
     }
-    /* 配置DHCP服务器 */
-    netif_p = netif_find(ifname);
-    if (netif_p == TD_NULL) {
-        (td_void)wifi_softap_disable();
+    /* 筛选扫描到的Wi-Fi网络，选择待连接的网络 */
+    for (bss_index = 0; bss_index < num; bss_index ++) {
+        if (strlen(expected_ssid) == strlen(result[bss_index].ssid)) {
+            if (memcmp(expected_ssid, result[bss_index].ssid, strlen(expected_ssid)) == 0) {
+                find_ap = TD_TRUE;
+                break;
+            }
+        }
+    }
+    /* 未找到待连接AP,可以继续尝试扫描或者退出 */
+    if (find_ap == TD_FALSE) {
+        osal_kfree(result);
         return -1;
     }
-    if (netifapi_netif_set_addr(netif_p, &st_ipaddr, &st_netmask, &st_gw) != 0) {
-        (td_void)wifi_softap_disable();
+    /* 找到网络后复制网络信息和接入密码 */
+    if (memcpy_s(expected_bss->ssid, WIFI_MAX_SSID_LEN, expected_ssid, strlen(expected_ssid)) != 0) {
+        osal_kfree(result);
         return -1;
     }
-    if (netifapi_dhcps_start(netif_p, NULL, 0) != 0) {
-        (td_void)wifi_softap_disable();
+    if (memcpy_s(expected_bss->bssid, WIFI_MAC_LEN, result[bss_index].bssid, WIFI_MAC_LEN) != 0) {
+        osal_kfree(result);
         return -1;
     }
-    PRINT("%s::SoftAp start success.\r\n", WIFI_SOFTAP_SAMPLE_LOG);
+    expected_bss->security_type = result[bss_index].security_type;
+    if (memcpy_s(expected_bss->pre_shared_key, WIFI_MAX_SSID_LEN, key, strlen(key)) != 0) {
+        osal_kfree(result);
+        return -1;
+    }
+    expected_bss->ip_type = 1; /* 1：IP类型为动态DHCP获取 */
+    osal_kfree(result);
     return 0;
 }
 
-int softap_sample_init(void *param)
+td_bool example_check_connect_status(td_void)
+{
+    td_u8 index;
+    wifi_linked_info_stru wifi_status;
+    /* 获取网络连接状态，共查询5次，每次间隔500ms */
+    for (index = 0; index < 5; index ++) {
+        (void)osDelay(50); /* 50: 延时500ms */
+        memset_s(&wifi_status, sizeof(wifi_linked_info_stru), 0, sizeof(wifi_linked_info_stru));
+        if (wifi_sta_get_ap_info(&wifi_status) != 0) {
+            continue;
+        }
+        if (wifi_status.conn_state == 1) {
+            return 0; /* 连接成功退出循环 */
+        }
+    }
+    return -1;
+}
+
+td_bool example_check_dhcp_status(struct netif *netif_p, td_u32 *wait_count)
+{
+    if ((ip_addr_isany(&(netif_p->ip_addr)) == 0) && (*wait_count <= WIFI_GET_IP_MAX_COUNT)) {
+        /* DHCP成功 */
+        PRINT("%s::STA DHCP success.\r\n", WIFI_STA_SAMPLE_LOG);
+        print_ip_address();  // 打印IP地址
+        return 0;
+    }
+
+    if (*wait_count > WIFI_GET_IP_MAX_COUNT) {
+        PRINT("%s::STA DHCP timeout, try again !.\r\n", WIFI_STA_SAMPLE_LOG);
+        *wait_count = 0;
+        g_wifi_state = WIFI_STA_SAMPLE_INIT;
+    }
+    return -1;
+}
+
+td_s32 example_sta_function(td_void)
+{
+    td_char ifname[WIFI_IFNAME_MAX_SIZE + 1] = "wlan0"; /* 创建的STA接口名 */
+    wifi_sta_config_stru expected_bss = {0}; /* 连接请求信息 */
+    struct netif *netif_p = TD_NULL;
+    td_u32 wait_count = 0;
+
+    /* 创建STA接口 */
+    if (wifi_sta_enable() != 0) {
+        return -1;
+    }
+    PRINT("%s::STA enable succ.\r\n", WIFI_STA_SAMPLE_LOG);
+
+    do {
+        (void)osDelay(1); /* 1: 等待10ms后判断状态 */
+        if (g_wifi_state == WIFI_STA_SAMPLE_INIT) {
+            PRINT("%s::Scan start!\r\n", WIFI_STA_SAMPLE_LOG);
+            g_wifi_state = WIFI_STA_SAMPLE_SCANING;
+            /* 启动STA扫描 */
+            if (wifi_sta_scan() != 0) {
+                g_wifi_state = WIFI_STA_SAMPLE_INIT;
+                continue;
+            }
+        } else if (g_wifi_state == WIFI_STA_SAMPLE_SCAN_DONE) {
+            /* 获取待连接的网络 */
+            if (example_get_match_network(&expected_bss) != 0) {
+                PRINT("%s::Do not find AP, try again !\r\n", WIFI_STA_SAMPLE_LOG);
+                g_wifi_state = WIFI_STA_SAMPLE_INIT;
+                continue;
+            }
+            g_wifi_state = WIFI_STA_SAMPLE_FOUND_TARGET;
+        } else if (g_wifi_state == WIFI_STA_SAMPLE_FOUND_TARGET) {
+            PRINT("%s::Connect start.\r\n", WIFI_STA_SAMPLE_LOG);
+            g_wifi_state = WIFI_STA_SAMPLE_CONNECTING;
+            /* 启动连接 */
+            if (wifi_sta_connect(&expected_bss) != 0) {
+                g_wifi_state = WIFI_STA_SAMPLE_INIT;
+                continue;
+            }
+        } else if (g_wifi_state == WIFI_STA_SAMPLE_CONNECT_DONE) {
+            PRINT("%s::DHCP start.\r\n", WIFI_STA_SAMPLE_LOG);
+            g_wifi_state = WIFI_STA_SAMPLE_GET_IP;
+            netif_p = netifapi_netif_find(ifname);
+            if (netif_p == TD_NULL || netifapi_dhcp_start(netif_p) != 0) {
+                PRINT("%s::find netif or start DHCP fail, try again !\r\n", WIFI_STA_SAMPLE_LOG);
+                g_wifi_state = WIFI_STA_SAMPLE_INIT;
+                continue;
+            }
+        } else if (g_wifi_state == WIFI_STA_SAMPLE_GET_IP) {
+            if (example_check_dhcp_status(netif_p, &wait_count) == 0) {
+                break;
+            }
+            wait_count++;
+        }
+    } while (1);
+
+    return 0;
+}
+
+int sta_sample_init(void *param)
 {
     param = param;
+
+    /* 注册事件回调 */
+    if (wifi_register_event_cb(&wifi_event_cb) != 0) {
+        PRINT("%s::wifi_event_cb register fail.\r\n", WIFI_STA_SAMPLE_LOG);
+        return -1;
+    }
+    PRINT("%s::wifi_event_cb register succ.\r\n", WIFI_STA_SAMPLE_LOG);
 
     /* 等待wifi初始化完成 */
     while (wifi_is_wifi_inited() == 0) {
         (void)osDelay(10); /* 1: 等待100ms后判断状态 */
     }
-    PRINT("%s::wifi init succ.\r\n", WIFI_SOFTAP_SAMPLE_LOG);
+    PRINT("%s::wifi init succ.\r\n", WIFI_STA_SAMPLE_LOG);
 
-    if (example_softap_function() != 0) {
-        PRINT("%s::example_softap_function fail.\r\n", WIFI_SOFTAP_SAMPLE_LOG);
+    if (example_sta_function() != 0) {
+        PRINT("%s::example_sta_function fail.\r\n", WIFI_STA_SAMPLE_LOG);
         return -1;
     }
     return 0;
 }
 
-static void softap_sample_entry(void)
+static void sta_sample_entry(void)
 {
     osThreadAttr_t attr;
-    attr.name       = "softap_sample_task";
+    attr.name       = "sta_sample_task";
     attr.attr_bits  = 0U;
     attr.cb_mem     = NULL;
     attr.cb_size    = 0U;
     attr.stack_mem  = NULL;
     attr.stack_size = WIFI_TASK_STACK_SIZE;
     attr.priority   = WIFI_TASK_PRIO;
-    if (osThreadNew((osThreadFunc_t)softap_sample_init, NULL, &attr) == NULL) {
-        PRINT("%s::Create softap_sample_task fail.\r\n", WIFI_SOFTAP_SAMPLE_LOG);
+    if (osThreadNew((osThreadFunc_t)sta_sample_init, NULL, &attr) == NULL) {
+        PRINT("%s::Create sta_sample_task fail.\r\n", WIFI_STA_SAMPLE_LOG);
     }
-    PRINT("%s::Create softap_sample_task succ.\r\n", WIFI_SOFTAP_SAMPLE_LOG);
+    PRINT("%s::Create sta_sample_task succ.\r\n", WIFI_STA_SAMPLE_LOG);
 }
 
 /* Run the sta_sample_task. */
-app_run(softap_sample_entry);
+app_run(sta_sample_entry);
