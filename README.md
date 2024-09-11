74958475a256051dc46f46b582952f46d8f4216a
tianrking <tian.r.king@gmail.com>
2024-09-11 10:40:41 +0800

    modified:   CMakeLists.txt 	modified:   README.md 	modified:   demo.c 	new file:   sle_uart_client/sle_uart_client.c 	new file:   sle_uart_client/sle_uart_client.h 	new file:   sle_uart_server/sle_uart_server.c 	new file:   sle_uart_server/sle_uart_server.h 	new file:   sle_uart_server/sle_uart_server_adv.c 	new file:   sle_uart_server/sle_uart_server_adv.h

--- README.md
+++ README.md
@@ -24,3 +24,14 @@ endif
 ```
 
 CLICK Kconfig Select APP -> Enable Sample -> Enable Demo
+
+SLE 30ms 全双工收发 2个长度 AA BB 没有问题  10ms 可能会死机
+发现只要数据量一大 就有接收延迟 似乎
+对于 90 个长度的 需要把延迟拉到 300ms 全双工才没有问题
+
+
+#define CONFIG_SLE_UART_BUS  0
+#define CONFIG_UART_TXD_PIN  17
+#define CONFIG_UART_RXD_PIN  18
+#define CONFIG_SAMPLE_SUPPORT_SLE_UART_SERVER 1
+// #define CONFIG_SAMPLE_SUPPORT_SLE_UART_CLIENT 1
--- demo.c
+++ demo.c
@@ -1,148 +1,255 @@
-#define CONFIG_SAMPLE_SUPPORT_BLE_UART_SERVER 
+#define CONFIG_SLE_UART_BUS  0
+#define CONFIG_UART_TXD_PIN  17
+#define CONFIG_UART_RXD_PIN  18
+#define CONFIG_SAMPLE_SUPPORT_SLE_UART_SERVER 1
+// #define CONFIG_SAMPLE_SUPPORT_SLE_UART_CLIENT 1
 
-#include "securec.h"
 #include "common_def.h"
 #include "soc_osal.h"
 #include "app_init.h"
+#include "pinctrl.h"
 #include "uart.h"
-#if defined(CONFIG_SAMPLE_SUPPORT_BLE_UART_SERVER)
-#define BLE_UART_TASK_STACK_SIZE            0x1200
-#include "bts_gatt_server.h"
-#include "ble_uart_server.h"
-#elif defined(CONFIG_SAMPLE_SUPPORT_BLE_UART_CLIENT)
-#define BLE_UART_TASK_STACK_SIZE            0x1200
-#include "bts_gatt_client.h"
-#include "ble_uart_client.h"
-#endif /* CONFIG_SAMPLE_SUPPORT_BLE_UART_CLIENT */
+// #include "pm_clock.h"
+#include "sle_low_latency.h"
+#if defined(CONFIG_SAMPLE_SUPPORT_SLE_UART_SERVER)
+#include "securec.h"
+#include "sle_uart_server.h"
+#include "sle_uart_server_adv.h"
+#include "sle_device_discovery.h"
+#include "sle_errcode.h"
+#elif defined(CONFIG_SAMPLE_SUPPORT_SLE_UART_CLIENT)
+#define SLE_UART_TASK_STACK_SIZE            0x600
+#include "sle_connection_manager.h"
+#include "sle_ssap_client.h"
+#include "sle_uart_client.h"
+#endif  /* CONFIG_SAMPLE_SUPPORT_SLE_UART_CLIENT */
 
-#define BLE_UART_TASK_PRIO                  28
-#define BLE_UART_BT_STACK_POWER_MS      10000
+#define SLE_UART_TASK_PRIO                  28
+#define SLE_UART_TASK_DURATION_MS           2000
+#define SLE_UART_BAUDRATE                   115200
+#define SLE_UART_TRANSFER_SIZE              512
 
-typedef struct {
-    uint8_t *value;
-    uint16_t value_len;
-} msg_data_t;
-unsigned long mouse_msg_queue = 0;
-unsigned int msg_rev_size = sizeof(msg_data_t);
+static uint8_t g_app_uart_rx_buff[SLE_UART_TRANSFER_SIZE] = { 0 };
 
-#if defined(CONFIG_SAMPLE_SUPPORT_BLE_UART_SERVER)
-static void ble_uart_read_int_handler(const void *buffer, uint16_t length, bool error)
+static uart_buffer_config_t g_app_uart_buffer_config = {
+    .rx_buffer = g_app_uart_rx_buff,
+    .rx_buffer_size = SLE_UART_TRANSFER_SIZE
+};
+
+static void uart_init_pin(void)
 {
-    osal_printk("ble_uart_read_int_handler server.\r\n");
-    unused(error);
-    if (ble_uart_get_connection_state() != 0) {
-        msg_data_t msg_data = { 0 };
-        void* buffer_cpy = osal_vmalloc(length);
-        if (memcpy_s(buffer_cpy, length, buffer, length) != EOK) {
-            osal_vfree(buffer_cpy);
-            return;
-        }
-        msg_data.value = (uint8_t *)buffer_cpy;
-        msg_data.value_len = length;
-        osal_msg_queue_write_copy(mouse_msg_queue, (void *)&msg_data, msg_rev_size, 0);
+    if (CONFIG_SLE_UART_BUS == 0) {
+        uapi_pin_set_mode(CONFIG_UART_TXD_PIN, PIN_MODE_1);
+        uapi_pin_set_mode(CONFIG_UART_RXD_PIN, PIN_MODE_1);       
+    }else if (CONFIG_SLE_UART_BUS == 1) {
+        uapi_pin_set_mode(CONFIG_UART_TXD_PIN, PIN_MODE_1);
+        uapi_pin_set_mode(CONFIG_UART_RXD_PIN, PIN_MODE_1);       
     }
 }
 
-static void *ble_uart_server_task(const char *arg)
+static void uart_init_config(void)
+{
+    uart_attr_t attr = {
+        .baud_rate = SLE_UART_BAUDRATE,
+        .data_bits = UART_DATA_BIT_8,
+        .stop_bits = UART_STOP_BIT_1,
+        .parity = UART_PARITY_NONE
+    };
+
+    uart_pin_config_t pin_config = {
+        .tx_pin = CONFIG_UART_TXD_PIN,
+        .rx_pin = CONFIG_UART_RXD_PIN,
+        .cts_pin = PIN_NONE,
+        .rts_pin = PIN_NONE
+    };
+    uapi_uart_deinit(CONFIG_SLE_UART_BUS);
+    uapi_uart_init(CONFIG_SLE_UART_BUS, &pin_config, &attr, NULL, &g_app_uart_buffer_config);
+
+}
+
+#if defined(CONFIG_SAMPLE_SUPPORT_SLE_UART_SERVER)
+#define SLE_UART_SERVER_DELAY_COUNT         5
+
+#define SLE_UART_TASK_STACK_SIZE            0x1200
+#define SLE_ADV_HANDLE_DEFAULT              1
+#define SLE_UART_SERVER_MSG_QUEUE_LEN       5
+#define SLE_UART_SERVER_MSG_QUEUE_MAX_SIZE  32
+#define SLE_UART_SERVER_QUEUE_DELAY         0xFFFFFFFF
+#define SLE_UART_SERVER_BUFF_MAX_SIZE       800
+
+unsigned long g_sle_uart_server_msgqueue_id;
+#define SLE_UART_SERVER_LOG                 "[sle uart server]"
+static void ssaps_server_read_request_cbk(uint8_t server_id, uint16_t conn_id, ssaps_req_read_cb_t *read_cb_para,
+    errcode_t status)
+{
+    osal_printk("%s ssaps read request cbk callback server_id:%x, conn_id:%x, handle:%x, status:%x\r\n",
+        SLE_UART_SERVER_LOG, server_id, conn_id, read_cb_para->handle, status);
+}
+static void ssaps_server_write_request_cbk(uint8_t server_id, uint16_t conn_id, ssaps_req_write_cb_t *write_cb_para,
+    errcode_t status)
+{
+    osal_printk("%s ssaps write request callback cbk server_id:%x, conn_id:%x, handle:%x, status:%x\r\n",
+        SLE_UART_SERVER_LOG, server_id, conn_id, write_cb_para->handle, status);
+    if ((write_cb_para->length > 0) && write_cb_para->value) {
+        osal_printk("\n sle uart recived data : %s\r\n", write_cb_para->value);
+        uapi_uart_write(CONFIG_SLE_UART_BUS, (uint8_t *)write_cb_para->value, write_cb_para->length, 0);
+    }
+}
+
+static void sle_uart_server_read_int_handler(const void *buffer, uint16_t length, bool error)
+{
+    unused(error);
+    if (sle_uart_client_is_connected()) {
+    sle_uart_server_send_report_by_handle(buffer, length);
+    } else {
+        osal_printk("%s sle client is not connected! \r\n", SLE_UART_SERVER_LOG);
+    }
+}
+
+
+static void sle_uart_server_create_msgqueue(void)
+{
+    if (osal_msg_queue_create("sle_uart_server_msgqueue", SLE_UART_SERVER_MSG_QUEUE_LEN, \
+        (unsigned long *)&g_sle_uart_server_msgqueue_id, 0, SLE_UART_SERVER_MSG_QUEUE_MAX_SIZE) != OSAL_SUCCESS) {
+        osal_printk("^%s sle_uart_server_create_msgqueue message queue create failed!\n", SLE_UART_SERVER_LOG);
+    }
+}
+
+static void sle_uart_server_delete_msgqueue(void)
+{
+    osal_msg_queue_delete(g_sle_uart_server_msgqueue_id);
+}
+
+static void sle_uart_server_write_msgqueue(uint8_t *buffer_addr, uint16_t buffer_size)
+{
+    osal_msg_queue_write_copy(g_sle_uart_server_msgqueue_id, (void *)buffer_addr, \
+                              (uint32_t)buffer_size, 0);
+}
+
+static int32_t sle_uart_server_receive_msgqueue(uint8_t *buffer_addr, uint32_t *buffer_size)
+{
+    return osal_msg_queue_read_copy(g_sle_uart_server_msgqueue_id, (void *)buffer_addr, \
+                                    buffer_size, SLE_UART_SERVER_QUEUE_DELAY);
+}
+static void sle_uart_server_rx_buf_init(uint8_t *buffer_addr, uint32_t *buffer_size)
+{
+    *buffer_size = SLE_UART_SERVER_MSG_QUEUE_MAX_SIZE;
+    (void)memset_s(buffer_addr, *buffer_size, 0, *buffer_size);
+}
+
+static void *sle_uart_server_task(const char *arg)
 {
     unused(arg);
-    ble_uart_server_init();
-    errcode_t ret = uapi_uart_register_rx_callback(CONFIG_BLE_UART_BUS,
-                                                   UART_RX_CONDITION_FULL_OR_SUFFICIENT_DATA_OR_IDLE,
-                                                   1, ble_uart_read_int_handler);
+    uint8_t rx_buf[SLE_UART_SERVER_MSG_QUEUE_MAX_SIZE] = {0};
+    uint32_t rx_length = SLE_UART_SERVER_MSG_QUEUE_MAX_SIZE;
+    uint8_t sle_connect_state[] = "sle_dis_connect";
+
+    sle_uart_server_create_msgqueue();
+    sle_uart_server_register_msg(sle_uart_server_write_msgqueue);
+    sle_uart_server_init(ssaps_server_read_request_cbk, ssaps_server_write_request_cbk);
+
+
+    /* UART pinmux. */
+    uart_init_pin();
+
+    /* UART init config. */
+    uart_init_config();
+
+    uapi_uart_unregister_rx_callback(CONFIG_SLE_UART_BUS);
+    errcode_t ret = uapi_uart_register_rx_callback(CONFIG_SLE_UART_BUS,
+                                                   UART_RX_CONDITION_FULL_OR_IDLE,
+                                                   1, sle_uart_server_read_int_handler);
+    if (ret != ERRCODE_SUCC) {
+        osal_printk("%s Register uart callback fail.[%x]\r\n", SLE_UART_SERVER_LOG, ret);
+        return NULL;
+    }
+    while (1) {
+        sle_uart_server_rx_buf_init(rx_buf, &rx_length);
+        sle_uart_server_receive_msgqueue(rx_buf, &rx_length);
+        if (strncmp((const char *)rx_buf, (const char *)sle_connect_state, sizeof(sle_connect_state)) == 0) {
+            ret = sle_start_announce(SLE_ADV_HANDLE_DEFAULT);
+            if (ret != ERRCODE_SLE_SUCCESS) {
+                osal_printk("%s sle_connect_state_changed_cbk,sle_start_announce fail :%02x\r\n",
+                    SLE_UART_SERVER_LOG, ret);
+            }
+        }
+        osal_msleep(SLE_UART_TASK_DURATION_MS);
+    }
+    sle_uart_server_delete_msgqueue();
+    return NULL;
+}
+#elif defined(CONFIG_SAMPLE_SUPPORT_SLE_UART_CLIENT)
+
+void sle_uart_notification_cb(uint8_t client_id, uint16_t conn_id, ssapc_handle_value_t *data,
+    errcode_t status)
+{
+    unused(client_id);
+    unused(conn_id);
+    unused(status);
+    osal_printk("\n sle uart recived data : %s\r\n", data->data);
+    uapi_uart_write(CONFIG_SLE_UART_BUS, (uint8_t *)(data->data), data->data_len, 0);
+}
+
+void sle_uart_indication_cb(uint8_t client_id, uint16_t conn_id, ssapc_handle_value_t *data,
+    errcode_t status)
+{
+    unused(client_id);
+    unused(conn_id);
+    unused(status);
+    osal_printk("\n sle uart recived data : %s\r\n", data->data);
+    uapi_uart_write(CONFIG_SLE_UART_BUS, (uint8_t *)(data->data), data->data_len, 0);
+}
+
+static void sle_uart_client_read_int_handler(const void *buffer, uint16_t length, bool error)
+{
+    unused(error);
+    ssapc_write_param_t *sle_uart_send_param = get_g_sle_uart_send_param();
+    uint16_t g_sle_uart_conn_id = get_g_sle_uart_conn_id();
+    sle_uart_send_param->data_len = length;
+    sle_uart_send_param->data = (uint8_t *)buffer;
+    ssapc_write_req(0, g_sle_uart_conn_id, sle_uart_send_param);
+}
+
+static void *sle_uart_client_task(const char *arg)
+{
+    unused(arg);
+    /* UART pinmux. */
+    uart_init_pin();
+
+    /* UART init config. */
+    uart_init_config();
+
+    uapi_uart_unregister_rx_callback(CONFIG_SLE_UART_BUS);
+    errcode_t ret = uapi_uart_register_rx_callback(CONFIG_SLE_UART_BUS,
+                                                   UART_RX_CONDITION_FULL_OR_IDLE,
+                                                   1, sle_uart_client_read_int_handler);
+    sle_uart_client_init(sle_uart_notification_cb, sle_uart_indication_cb);
+    
     if (ret != ERRCODE_SUCC) {
         osal_printk("Register uart callback fail.");
         return NULL;
     }
-    while (1) {
-        msg_data_t msg_data = { 0 };
-        int msg_ret = osal_msg_queue_read_copy(mouse_msg_queue, &msg_data, &msg_rev_size, OSAL_WAIT_FOREVER);
-        if (msg_ret != OSAL_SUCCESS) {
-            osal_printk("msg queue read copy fail.");
-            if (msg_data.value != NULL) {
-                osal_vfree(msg_data.value);
-            }
-            continue;
-        }
-        if (msg_data.value != NULL) {
-            ble_uart_server_send_input_report(msg_data.value, msg_data.value_len);
-            osal_vfree(msg_data.value);
-        }
-    }
+
     return NULL;
 }
-#elif defined(CONFIG_SAMPLE_SUPPORT_BLE_UART_CLIENT)
-static void ble_uart_read_int_handler(const void *buffer, uint16_t length, bool error)
-{
-    osal_printk("ble_uart_read_int_handler client.\r\n");
-    unused(error);
-    msg_data_t msg_data = { 0 };
-    void* buffer_cpy = osal_vmalloc(length);
-    if (memcpy_s(buffer_cpy, length, buffer, length) != EOK) {
-        osal_vfree(buffer_cpy);
-        return;
-    }
-    msg_data.value = (uint8_t *)buffer_cpy;
-    msg_data.value_len = length;
-    osal_msg_queue_write_copy(mouse_msg_queue, (void *)&msg_data, msg_rev_size, 0);
-}
+#endif  /* CONFIG_SAMPLE_SUPPORT_SLE_UART_CLIENT */
 
-static void *ble_uart_client_task(const char *arg)
+static void sle_uart_entry(void)
 {
-    unused(arg);
-    osal_printk("ble_uart_client_task entry.");
-    ble_uart_client_init();
-    errcode_t ret = uapi_uart_register_rx_callback(CONFIG_BLE_UART_BUS,
-                                                   UART_RX_CONDITION_FULL_OR_SUFFICIENT_DATA_OR_IDLE,
-                                                   1, ble_uart_read_int_handler);
-    if (ret != ERRCODE_SUCC) {
-        osal_printk("Register uart callback fail.");
-        return NULL;
-    }
-    while (1) {
-        msg_data_t msg_data = { 0 };
-        int msg_ret = osal_msg_queue_read_copy(mouse_msg_queue, &msg_data, &msg_rev_size, OSAL_WAIT_FOREVER);
-        if (msg_ret != OSAL_SUCCESS) {
-            osal_printk("msg queue read copy fail.");
-            if (msg_data.value != NULL) {
-                osal_vfree(msg_data.value);
-            }
-            continue;
-        }
-        if (msg_data.value != NULL) {
-            uint16_t write_handle = ble_uart_get_write_vlaue_handle();
-            ble_uart_client_write_cmd(msg_data.value, msg_data.value_len, write_handle);
-            osal_vfree(msg_data.value);
-        }
-    }
-    return NULL;
-}
-#endif  /* CONFIG_SAMPLE_SUPPORT_BLE_UART_CLIENT */
-
-static void ble_uart_entry(void)
-{
-    // char *arg = NULL;
-    int msg_ret = osal_msg_queue_create("task_msg", msg_rev_size, &mouse_msg_queue, 0, msg_rev_size);
-    if (msg_ret != OSAL_SUCCESS) {
-        osal_printk("msg queue create fail.");
-        return;
-    }
     osal_task *task_handle = NULL;
     osal_kthread_lock();
-#if defined(CONFIG_SAMPLE_SUPPORT_BLE_UART_SERVER)
-    // ble_uart_server_task(arg);
-    task_handle = osal_kthread_create((osal_kthread_handler)ble_uart_server_task, 0, "BLEUartServerTask",
-                                      BLE_UART_TASK_STACK_SIZE);
-#elif defined(CONFIG_SAMPLE_SUPPORT_BLE_UART_CLIENT)
-    // ble_uart_client_task(arg);
-    task_handle = osal_kthread_create((osal_kthread_handler)ble_uart_client_task, 0, "BLEUartClientTask",
-                                      BLE_UART_TASK_STACK_SIZE);
-#endif /* CONFIG_SAMPLE_SUPPORT_BLE_UART_CLIENT */
+#if defined(CONFIG_SAMPLE_SUPPORT_SLE_UART_SERVER)
+    task_handle = osal_kthread_create((osal_kthread_handler)sle_uart_server_task, 0, "SLEUartServerTask",
+                                      SLE_UART_TASK_STACK_SIZE);
+#elif defined(CONFIG_SAMPLE_SUPPORT_SLE_UART_CLIENT)
+    task_handle = osal_kthread_create((osal_kthread_handler)sle_uart_client_task, 0, "SLEUartDongleTask",
+                                      SLE_UART_TASK_STACK_SIZE);
+#endif /* CONFIG_SAMPLE_SUPPORT_SLE_UART_CLIENT */
     if (task_handle != NULL) {
-        osal_kthread_set_priority(task_handle, BLE_UART_TASK_PRIO);
+        osal_kthread_set_priority(task_handle, SLE_UART_TASK_PRIO);
     }
     osal_kthread_unlock();
 }
 
-/* Run the ble_uart_entry. */
-app_run(ble_uart_entry);
+/* Run the sle_uart_entry. */
+app_run(sle_uart_entry);
